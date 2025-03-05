78b452d393453ed24786edb2989d69805df68058
Clon54 <clon5454@outlook.com>
2025-03-04 22:38:51 -0500

    Define la interfaz Scene.

---
+++ include/Scene/Scene.h
@@ -0,0 +1,13 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+class Scene
+{
+public:
+    virtual void Init() = 0;
+    virtual void Draw() = 0;
+    virtual void Update() = 0;
+    virtual void Input() = 0;
+};
+
+#endif
