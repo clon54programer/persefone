c5f7d6be15ae9bf0b2286fd06478572657e70efc
Clon54 <clon5454@outlook.com>
2025-03-05 18:42:49 -0500

    Agregue la documentacion a la clase GameObject.

--- include/GameObject/GameObject.h
+++ include/GameObject/GameObject.h
@@ -1,6 +1,12 @@
 #ifndef GAME_OBJECT_H
 #define GAME_OBJECT_H
 
+/// @brief Es una clase abstracta la
+/// cual debe heredar todos las clases.
+/// @attention Al ser una clase abstracta,
+/// la clase hija que herede esta clase,
+/// debe implementar los metodos
+/// Input, Update y Draw.
 class GameObject
 {
 public:
