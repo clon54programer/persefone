be83ba2efbcf4b9e314e1eca49ac7309849d7fa3
Clon54 <clon5454@outlook.com>
2025-03-04 23:09:21 -0500

    Agregue los archivos para trabajar con conan.

---
+++ libs/conanfile.py
@@ -0,0 +1,14 @@
+from conan import ConanFile
+from conan.tools.scons import SConsDeps
+
+
+class Pkg(ConanFile):
+    generators = "SConsDeps"
+
+
+class App(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+
+    def generate(self):
+        tc = SConsDeps(self)
+        tc.generate()
