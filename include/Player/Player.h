db9a1cebbfad2afa2ad4866660000cf983bcc16c
Clon54 <clon5454@outlook.com>
2025-03-05 19:03:08 -0500

    Cree los destructores y el constructor, ademas de un getter para el mienbro position.

--- include/Player/Player.h
+++ include/Player/Player.h
@@ -14,9 +14,15 @@ private:
     float speed_run{0.0f};
 
 public:
+    Player() = default;
+    ~Player() = default;
+
     void Input() override;
     void Update() override;
     void Draw() const override;
+
+    /// Getters
+    Vector2 GetPosition() const { return this->position; }
 };
 
 #endif
