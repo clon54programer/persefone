0148954368f2983fab3af26da28314946ab1dffe
Clon54 <clon5454@outlook.com>
2025-03-05 19:38:19 -0500

    Agregue las teclas para mover al jugador.

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,6 +2,31 @@
 
 void Player::Input()
 {
+    /// Init Y eje
+    if (IsKeyUp(KEY_UP))
+    {
+        this->position.y += 1;
+    }
+
+    if (IsKeyUp(KEY_DOWN))
+    {
+        this->position.y -= 1;
+    }
+
+    /// End Y eje
+
+    /// Init X eje
+    if (IsKeyUp(KEY_LEFT))
+    {
+        this->position.x += 1;
+    }
+
+    if (IsKeyUp(KEY_RIGHT))
+    {
+        this->position.x -= 1;
+    }
+
+    /// End X eje
 }
 
 void Player::Update()
