8c2d26915de75cf2a1f5ee8a2731b43962a08653
Clon54 <clon5454@outlook.com>
2025-03-05 20:54:58 -0500

    Actualize el script de compilacion.

--- build.bat
+++ build.bat
@@ -6,9 +6,11 @@ set main_function= src\main.cpp
 
 set flags= -Iinclude\
 
+g++ -c -o Camera.o src\Camera.cpp %flags%
 g++ -c -o Player.o src\Player.cpp %flags%
 
-set source= Player.o
+
+set source= Player.o Camera.o
 
 
 g++ -o %name_executable% %main_function%  %source%  %dependecy% %flags%
